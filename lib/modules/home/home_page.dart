390391f7e7fb2698b3da5896dc49c65e7d663f69
Yansb <yansbarreiro@gmail.com>
2021-07-29 00:04:38 -0300

    feat: get boleto with cam

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -67,7 +67,9 @@ class _HomePageState extends State<HomePage> {
                     color: AppColors.primary,
                   )),
               GestureDetector(
-                onTap: () => print("clicou"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/barcode_scanner");
+                },
                 child: Container(
                     height: 56,
                     width: 56,
@@ -75,7 +77,9 @@ class _HomePageState extends State<HomePage> {
                         color: AppColors.primary,
                         borderRadius: BorderRadius.circular(5)),
                     child: IconButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/barcode_scanner");
+                        },
                         icon: Icon(
                           Icons.add_box_outlined,
                           color: AppColors.background,
